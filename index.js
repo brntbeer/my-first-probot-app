e6b4678ce9fe3253e251fbe1bcabb46ca8e8b205
Brent Beer <brent.beer@gmail.com>
2018-07-30 15:46:31 +0200

    suggestions on issues

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var recastai = require('recastai').default
+
+
 module.exports = app => {
   // Your code here
   app.log('Yay, the app was loaded!')
@@ -10,8 +13,26 @@ module.exports = app => {
 }
 
 module.exports = robot => {
-  robot.on('issues.opened', async context => {
-    // An issue was just opened.
-    robot.log(context)
-  })
+  var request = new recastai.request(process.env.RECASTAI_TOKEN)
+
+      request.analyseText(context.payload.issue.title)
+      .then(function(res) {
+        var labels = []
+
+        if(res.isAssert()) {
+          labels.push('bug')
+        }
+        if(res.isCommand()) {
+          labels.push('enhancement')
+        }
+
+        let params = context.issue({
+          labels: labels
+        })
+        return context.github.issues.addLabels(params)
+      })
+      .catch(function(err) {
+        app.log(err)
+      })
+    })
 }
